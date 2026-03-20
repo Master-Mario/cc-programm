b177027fdb7de011ac9f7cc340d9f2b0dcae2b09
MasterMario <65150666+Master-Mario@users.noreply.github.com>
2026-03-20 12:27:10 +0100

    Update version and fix cursor position method

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-print("Main v0.2.3 gestartet")
+print("Main v0.2.4 gestartet")
 peripheral.wrap("left").clear()
-peripheral.setCursorPos(0, 0)
+peripheral.wrap("left").setCursorPos(0, 0)
 peripheral.wrap("left").write("MAC-Netzwerk")
